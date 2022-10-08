f07649dcaa45fd857f5f0d52076c7e6753cbb881
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2022-10-08 03:45:37 -0500

    Filtros sal y pimienta y mediana (Simulink)
    Creación del programa que agrega los filtros sal y pimienta y mediana al video capturado por la cámara.

---
+++ Práctica 7 - Filtros sal y pimienta y mediana (Simulink)/salpimienta.m
@@ -0,0 +1,20 @@
+function resultado = salpimienta (imagen)
+    dimensionMascara = 1;
+    factorDeGranulado = 1;
+    resultado = imagen;
+    mitadMascara = round(dimensionMascara / 2);
+    colores = [-50 50];
+    [ancho, alto] = size(imagen);
+    for color = 1:2
+        for cantidad = 1:round((ancho * alto) * (factorDeGranulado / 1000))
+            x = randi([mitadMascara (ancho - mitadMascara)],1 , 1);
+            y = randi([mitadMascara (alto - mitadMascara)], 1, 1);
+            resultado(x,y) = colores(color);
+            for i = 1:dimensionMascara
+                for j = 1: dimensionMascara
+                    resultado((x - (mitadMascara - 1)) + i, (y - (mitadMascara - 1)) + j) = resultado((x - (mitadMascara - 1)) + i, (y - (mitadMascara - 1)) + j) + colores(color);
+                end
+            end
+        end
+    end
+end
